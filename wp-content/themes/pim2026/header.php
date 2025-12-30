9f42a1370d7b6cac2b46fa19c4f214f1ca6c00e9
Kay Li <kayli@Kays-MacBook-Pro.local>
2025-12-30 21:34:51 +0800

    .

--- wp-content/themes/pim2026/header.php
+++ wp-content/themes/pim2026/header.php
@@ -33,7 +33,7 @@
 	</a>
 
 	<?php //get_template_part( 'template-parts/header/site-header' ); ?>
-	<div class="header-submenu-container" style="display:none; position:absolute; left:0; top:100%; width:100vw; margin-left:calc(50% - 50vw); background:#fff; z-index:1000; border-top:1px solid #eee; box-shadow:0 6px 25px rgba(0,0,0,0.09);">
+	<div class="header-submenu-container" style="display:none; position:absolute; left:0; top:100px; width:100vw; margin-left:calc(50% - 50vw); background:#fff; z-index:1000; border-top:1px solid #eee; box-shadow:0 6px 25px rgba(0,0,0,0.09);">
 								<div class="container py-4">
 									<ul class="header-submenu list-unstyled mb-0 d-flex flex-wrap gap-4">
 										<li><a href="#">Call for Papers</a></li>
