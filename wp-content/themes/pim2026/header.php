7332b62ef132fe526b8b76453c2bb5003e745334
Kay Li <kayli@Kays-MacBook-Pro.local>
2025-12-30 20:02:27 +0800

    .

--- wp-content/themes/pim2026/header.php
+++ wp-content/themes/pim2026/header.php
@@ -32,56 +32,8 @@
 
 	<?php //get_template_part( 'template-parts/header/site-header' ); ?>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php if ( has_custom_logo() ) : ?>
-				<div class="site-logo"><?php the_custom_logo(); ?></div>
-			<?php else : ?>
-				<?php
-				$blog_info = get_bloginfo( 'name' );
-				if ( $blog_info ) :
-					?>
-					<?php if ( is_front_page() && ! is_paged() ) : ?>
-						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></h1>
-					<?php else : ?>
-						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( $blog_info ); ?></a></p>
-					<?php endif; ?>
-				<?php endif; ?>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
-		<?php if ( has_nav_menu( 'primary' ) ) : ?>
-			<nav id="site-navigation" class="primary-navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
-				<div class="menu-button-container">
-					<button id="primary-mobile-menu" class="button" aria-controls="primary-menu-list" aria-expanded="false">
-						<span class="dropdown-icon open"><?php esc_html_e( 'Menu', 'twentytwentyone' ); ?>
-							<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'menu' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
-						</span>
-						<span class="dropdown-icon close"><?php esc_html_e( 'Close', 'twentytwentyone' ); ?>
-							<?php echo twenty_twenty_one_get_icon_svg( 'ui', 'close' ); // phpcs:ignore WordPress.Security.EscapeOutput ?>
-						</span>
-					</button><!-- #primary-mobile-menu -->
-				</div><!-- .menu-button-container -->
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location'  => 'primary',
-						'menu_class'      => 'menu-wrapper',
-						'container_class' => 'primary-menu-container',
-						'items_wrap'      => '<ul id="primary-menu-list" class="%2$s">%3$s</ul>',
-						'fallback_cb'     => false,
-					)
-				);
-				?>
-			</nav><!-- #site-navigation -->
-		<?php else : ?>
-			<nav id="site-navigation" class="primary-navigation" aria-label="<?php esc_attr_e( 'Primary menu', 'twentytwentyone' ); ?>">
-				<ul class="menu-wrapper">
-					<li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e( 'Home', 'twentytwentyone' ); ?></a></li>
-				</ul>
-			</nav>
-		<?php endif; ?>
-	</header><!-- #masthead -->
+	<div class=""></div>
 
 
 	<div id="content" class="site-content">
