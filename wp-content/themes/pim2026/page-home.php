a4c04ed4d3d5d85c6ce0692b50e07ffa0272649b
Kay Li <kayli@Kays-MacBook-Pro.local>
2025-12-30 21:27:18 +0800

    .

--- wp-content/themes/pim2026/page-home.php
+++ wp-content/themes/pim2026/page-home.php
@@ -118,8 +118,8 @@ get_header(); ?>
 
 
 
-<div class="mt-5"></div><div class="mt-5"></div><div class="mt-5"></div><div class="mt-5"></div>
-<div class="container">
+
+<div class="container mt-5">
 
 <div class="row justify-content-center my-5">
   <div class="col-12 col-md-10 col-lg-8">
