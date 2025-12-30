cdef5ee87324191be176ca907ba0c5db3ad8191b
Kay Li <kayli@Kays-MacBook-Pro.local>
2025-12-30 22:47:34 +0800

    .

--- wp-content/themes/pim2026/header.php
+++ wp-content/themes/pim2026/header.php
@@ -49,7 +49,7 @@
 									<div class="col-md-4 col-lg-4 ">
 	<h6>Getting there</h6>
 	<table><tr>
-		<td>
+		<td class="align-top">
 <img class="cube" src="<?php echo get_template_directory_uri();?>/assets/images/cube.jpg" alt="">
 
 		</td><td>
