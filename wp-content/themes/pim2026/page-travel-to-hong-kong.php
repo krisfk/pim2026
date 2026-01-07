bfa0e9ab84588f60b56c97a2b3944df9b4ccec7b
Kay Li <kayli@Kays-MacBook-Pro.local>
2026-01-07 16:33:47 +0800

    .

---
+++ wp-content/themes/pim2026/page-travel-to-hong-kong.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+get_header(); ?>
+
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
+<?php endif; ?>
+
+<div class="subpage-banner-full">
+  <img
+    src="<?php echo get_template_directory_uri(); ?>/assets/images/subpage-banner.jpg"
+    alt="CUHK Highlights Banner"
+    class="subpage-banner-img"
+  >
+</div>
+
+
+
+
+
+<div class="container text-center middle-container">
+
+	<h1 class="mt-5 mb-3 fw-bold">Travel to Hong Kong</h1>
+
+
+   
+
+
+
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    const btnGroup = document.getElementById('submenu-btn-group');
+    if (!btnGroup) return;
+    btnGroup.querySelectorAll('.submenu-btn').forEach(function(btn) {
+      btn.addEventListener('click', function(e) {
+        btnGroup.querySelectorAll('.submenu-btn').forEach(function(b){b.classList.remove('active')});
+        this.classList.add('active');
+      });
+    });
+  });
+</script>
+</div>	
+
+
+
+
+
+
+
+<?php
+
+
+get_footer();
