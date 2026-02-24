9429e232ed23b65a97269a9def5a2709f39c56ab
Kay Li <krisfk@gmail.com>
2026-02-25 04:15:58 +0800

    .

--- wp-content/themes/pim2026/footer.php
+++ wp-content/themes/pim2026/footer.php
@@ -114,20 +114,21 @@ Cheng Yu Tung Building, 12 Chak Cheung Street, Shatin, N.T., Hong Kong
 					<div class="row">
 						<div class="col-6">
 							<ul class="list-unstyled">
-								<li><a href="<?php echo home_url(); ?>" class="footer-link">Conference overview</a></li>
-								<li><a href="<?php echo home_url('/schedule'); ?>" class="footer-link">Schedule</a></li>
+								<li><a href="<?php echo home_url(); ?>" class="footer-link">Overview</a></li>
+								<li><a href="<?php echo home_url('/schedule'); ?>" class="footer-link">The Programme</a></li>
 								<li><a href="<?php echo home_url('/registration'); ?>" class="footer-link">Register now</a></li>
-								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Travel to HK</a></li>
-								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Accommodation</a></li>
+								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Travel to Hong Kong</a></li>
+								<li><a href="<?php echo home_url('/accommodation'); ?>" class="footer-link">Accommodation</a></li>
 							</ul>
 						</div>
 						<div class="col-6">
 							<ul class="list-unstyled">
-								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Tours</a></li>
-								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">About HK</a></li>
+								<!-- <li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Tours</a></li> -->
+								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Discover Hong Kong
+								</a></li>
 								<li><a href="<?php echo home_url('/cuhk-highlights'); ?>" class="footer-link">CUHK highlights</a></li>
 								<li><a href="#" onclick="window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' }); return false;" class="footer-link">Contact</a></li>
-								<li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Logistics</a></li>
+								<!-- <li><a href="<?php echo home_url('/travel-to-hong-kong'); ?>" class="footer-link">Logistics</a></li> -->
 							</ul>
 						</div>
 					</div>
