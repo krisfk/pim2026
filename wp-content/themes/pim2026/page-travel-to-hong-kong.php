d033354c778c8d91e7955c1cc66abd4c0c53353b
Kay Li <krisfk@gmail.com>
2026-03-11 02:07:27 +0800

    .

--- wp-content/themes/pim2026/page-travel-to-hong-kong.php
+++ wp-content/themes/pim2026/page-travel-to-hong-kong.php
@@ -88,7 +88,7 @@ get_header(); ?>
       </li>
       <li class="mt-2">
         <strong>MTR:</strong> 
-        approximately 66 minutes (MTR trip planner) <br>
+        approximately 66 minutes <a href="https://www.mtr.com.hk/en/customer/jp/index.php?oLabel=Airport&oType=HRStation&oValue=47&dLabel=University&dType=HRStation&dValue=71" target="_blank"> (MTR trip planner)</a> <br>
 Route A: 2 interchanges, 15 minutes walkingAirport Station -> Hong Kong Station -> Central Station -> Admiralty Station -> University Station
 <br>
 Route B: 3 interchanges, 9 minutes walkingAirport Station -> Tsing Yi Station -> Nam Cheong Station -> Hung Hom Station -> University Station
