c946bf458483209475b446884b9e7c0011742a53
Kay Li <krisfk@gmail.com>
2026-06-16 19:45:18 +0800

    .

--- wp-content/themes/pim2026/page-optional-tour.php
+++ wp-content/themes/pim2026/page-optional-tour.php
@@ -100,7 +100,10 @@ font-weight:bold;
   }
   @media (max-width: 767.98px) {
     .lightbox-img { max-width: 98vw; max-height: 98vh; }
-    .lightbox-close-btn { top: 12px; right: 10px; font-size: 1.7rem; }
+    .lightbox-close-btn { top: 12px; right: 10px; font-size: 1.7rem;
+    position: relative;
+    top:3rem;
+    }
   }
 </style>
 
