aeeabde753f3e6a4a59e6bc3d9287d804b838655
Kay Li <krisfk@gmail.com>
2026-06-16 19:17:15 +0800

    .

--- wp-content/themes/pim2026/page-optional-tour.php
+++ wp-content/themes/pim2026/page-optional-tour.php
@@ -169,9 +169,10 @@ font-weight:bold;
 
                         <div class="tour-photos">
 
-                        <img class="w-100" src="https://2026cuhkpim.com/wp-content/uploads/2026/06/o1.png" alt=""> <br>
-                        <img class="w-100" src="https://2026cuhkpim.com/wp-content/uploads/2026/06/o2.png" alt=""><br>
-                        <img class="w-100" src="https://2026cuhkpim.com/wp-content/uploads/2026/06/o3.png" alt=""><br>
+                        <ul class="list-unstyled">
+                        <li><img class="w-100" src="https://2026cuhkpim.com/wp-content/uploads/2026/06/o1.png" alt=""></li>
+                          <li><img class="w-100" src="https://2026cuhkpim.com/wp-content/uploads/2026/06/o2.png" alt=""></li>
+                        </ul>
                         </div>
                     </td>
                     <td class="schedule-col-session">
