319467dfef0fc5f5e0cfa5a2647f43af9c5a5e12
Kay Li <krisfk@gmail.com>
2026-06-16 19:10:06 +0800

    .

--- wp-content/themes/pim2026/header.php
+++ wp-content/themes/pim2026/header.php
@@ -299,7 +299,7 @@
 							<td class="align-top pe-2">
 							<img class="cube" src="<?php echo get_template_directory_uri();?>/assets/images/cube.jpg" alt="">
 							</td><td>
-							<a href="<?php echo home_url('/discover-hong-kong'); ?>" >
+							<a href="<?php echo home_url('/optional-tour'); ?>" >
 								<span class="fw-bold">Optional Tour</span> <br>
 								<!-- Reach out with questions or concerns -->
 							</a>
