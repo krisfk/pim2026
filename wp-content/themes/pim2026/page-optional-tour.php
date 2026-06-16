24a6d096e944accbc316e857e75921737b02c82b
Kay Li <krisfk@gmail.com>
2026-06-16 18:09:30 +0800

    .

--- wp-content/themes/pim2026/page-optional-tour.php
+++ wp-content/themes/pim2026/page-optional-tour.php
@@ -52,32 +52,32 @@ get_header(); ?>
 
     
     <div class="table-responsive my-4 text-start">
-    <table>
+    <table class="table table-bordered schedule-programme-table mb-0">
         <thead>
             <tr>
-                <th style="width: 18%;">Tour option</th>
-                <th style="width: 8%;">Capacity</th>
-                <th style="width: 12%;">Estimated Cost</th>
-                <th style="width: 37%;">Overview</th>
-                <th style="width: 25%;">Remarks</th>
+                <th class="schedule-col-time" scope="col" style="width: 18%;">Tour option</th>
+                <th class="schedule-col-session" scope="col" style="width: 8%;">Capacity</th>
+                <th class="schedule-col-theme" scope="col" style="width: 12%;">Estimated Cost</th>
+                <th class="schedule-col-venue" scope="col" style="width: 37%;">Overview</th>
+                <th class="schedule-col-venue" scope="col" style="width: 25%;">Remarks</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>
+                <td class="schedule-col-time">
                     <div class="tour-title">1. Old Town Central Walking Tour</div>
                     <div class="tour-tagline">“Best for first-time visitors & cultural immersion”</div>
                     <div class="tour-duration">Duration: 2 hours</div>
                 </td>
-                <td>
+                <td class="schedule-col-session">
                     Min. 17<br>
                     Max. 40
                 </td>
-                <td>
+                <td class="schedule-col-theme">
                     HKD470<br>
                     (~USD60)
                 </td>
-                <td>
+                <td class="schedule-col-venue">
                     <p style="margin-top: 0;">Explore the rich contrasts of Hong Kong through this guided walking tour of Central and Sheung Wan. This experience takes you through a dynamic urban landscape where heritage buildings, traditional trades, and historic temples coexist with contemporary architecture and modern city life. Discover stories and local perspectives beyond typical guidebooks, offering an authentic introduction to Hong Kong’s cultural and social fabric.</p>
                     <div class="highlights-title">Highlights:</div>
                     <ul>
@@ -89,7 +89,7 @@ get_header(); ?>
                         <li><a href="#">Kwong Wah Printing Company</a> (1-hour Letterpress Workshop)</li>
                     </ul>
                 </td>
-                <td>
+                <td class="schedule-col-venue">
                     <ul>
                         <li><strong>Assembly Time:</strong> 10:00am (TBC)</li>
                         <li><strong>Assembly Point:</strong> Central Market</li>
@@ -100,20 +100,20 @@ get_header(); ?>
             </tr>
             
             <tr>
-                <td>
+                <td class="schedule-col-time">
                     <div class="tour-title">2. Lantau Monastery & Tai O Tour</div>
                     <div class="tour-tagline">“Best for scenic + iconic Hong Kong landmarks”</div>
                     <div class="tour-duration">Duration: 5 hours</div>
                 </td>
-                <td>
+                <td class="schedule-col-session">
                     Min. 12<br>
                     Max. 22
                 </td>
-                <td>
+                <td class="schedule-col-theme">
                     HKD980<br>
                     (~USD126)
                 </td>
-                <td>
+                <td class="schedule-col-venue">
                     <p style="margin-top: 0;">Take in the natural scenery and cultural heritage of Lantau Island on this half-day tour. Begin with a scenic 5.7 km Ngong Ping Cable Car ride offering panoramic views of mountains and coastline. Visit the tranquil Po Lin Monastery and the iconic Tian Tan Buddha. The tour concludes at Tai O Fishing Village, known for its stilt houses, local shops, and traditional way of life.</p>
                     <div class="highlights-title">Highlights:</div>
                     <ul>
@@ -123,10 +123,10 @@ get_header(); ?>
                         <li><a href="#">Tai O Village</a></li>
                     </ul>
                 </td>
-                <td>
+                <td class="schedule-col-venue">
                     <ul>
                         <li><strong>Assembly Time:</strong> 09:00am (TBC)</li>
-                        <li><strong>Assembly & Dismissal Point:</strong> Sheraton Hong Kong Hotel & Towers Lobby (Location&gt;&gt; <a href="https://maps.app.goo.gl/y2MMW92n6YBARvsu7" target="_blank">https://maps.app.goo.gl/y2MMW92n6YBARvsu7</a>)</li>
+                        <li><strong>Assembly & Dismissal Point:</strong> Sheraton Hong Kong Hotel & Towers Lobby (Location>> <a href="https://maps.app.goo.gl/y2MMW92n6YBARvsu7" target="_blank">https://maps.app.goo.gl/y2MMW92n6YBARvsu7</a>)</li>
                         <li>Air-conditioned coach transport throughout the tour</li>
                         <li><strong>Participants are required to arrange their own transportation to the assembly point</strong></li>
                     </ul>
