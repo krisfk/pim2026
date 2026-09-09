641046c8a4eb162338aa6d75f209164dd736e155
Kay Li <krisfk@gmail.com>
2026-09-09 14:31:57 +0800

    .

--- wp-content/themes/pim2026/page-schedule.php
+++ wp-content/themes/pim2026/page-schedule.php
@@ -150,8 +150,8 @@ get_header(); ?>
 			<tbody>
 				<tr>
 					<td class="schedule-col-time">10:30&ndash;12:00</td>
-					<td class="schedule-col-session"></td>
-					<td class="schedule-col-theme">Registration opens</td>
+					<td colspan="2" class="schedule-col-session"></td>
+					<!-- <td class="schedule-col-theme">Registration opens</td> -->
 					<td class="schedule-col-venue">Registration counter (Ballroom entrance)</td>
 				</tr>
 				<tr>
